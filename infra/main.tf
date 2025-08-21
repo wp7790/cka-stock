d7919e97d52a5d9102c4b650d2ffac2a025f665a
Yonas Ayele <yonasayele@hotmail.com>
2025-08-21 14:26:41 -0400

    Add GKE cluster resources

--- infra/main.tf
+++ infra/main.tf
@@ -40,3 +40,27 @@ resource "google_container_node_pool" "primary_nodes" {
     ]
   }
 }
+
+resource "google_container_cluster" "primary" {
+  name     = "cka-gke"
+  location = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
+
