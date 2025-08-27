f81be569c276205891b65a73ff30da35db81ee3a
wp7790 <76229097+wp7790@users.noreply.github.com>
2025-08-27 14:03:59 -0400

    Delete infra directory

--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "kubernetes_cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubernetes_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
--- infra/provider.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "cka-tf-state-wp23"
-    prefix = "gke-cluster"
-  }
-}
