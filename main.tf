15538d920c91442a74f44c39dacd280ee34c6d44
wp7790 <76229097+wp7790@users.noreply.github.com>
2025-08-27 14:06:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
 
 resource "google_container_cluster" "primary" {
   name     = "cka-gke"
