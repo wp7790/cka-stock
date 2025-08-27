e63ff1e68669d9b7ebfa6d654f1533b18434ad3e
wp7790 <76229097+wp7790@users.noreply.github.com>
2025-08-27 14:05:39 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,18 @@
 terraform {
-  backend "gcs" {
-    bucket = "cka-tf-state-wp23"
-    prefix = "gke-cluster"
+  required_version = ">= 1.5"
+
+  backend "remote" {
+    organization = "cka-stock"  # Your Terraform Cloud org
+
+    workspaces {
+      name = "cka-stock"         # Your workspace name in Terraform Cloud
+    }
   }
+}
+
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
 }
