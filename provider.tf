e9997fc60065194274d351bb797f372ab537b84a
wp7790 <76229097+wp7790@users.noreply.github.com>
2025-08-28 12:02:20 -0400

    update provider

--- provider.tf
+++ provider.tf
@@ -8,6 +8,22 @@ terraform {
       name = "cka-stock"         # Your workspace name in Terraform Cloud
     }
   }
+
+reqired_providers {
+  google = {
+    source = "hashicorp/google"
+    version = "~> 6.0"
+  }
+  kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+}
+
 }
 
 provider "google" {
@@ -16,3 +32,25 @@ provider "google" {
   region      = var.region
   zone        = var.zone
 }
+
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+    )
+  }
+}
+
