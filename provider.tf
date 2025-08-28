06475e4cfb92b65f6d3416e2fe69aa7f4a239eac
wp7790 <76229097+wp7790@users.noreply.github.com>
2025-08-28 12:20:47 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,31 @@
+# Declares the required providers for Terraform to download and use.
 terraform {
   required_version = ">= 1.5"
 
   backend "remote" {
-    organization = "cka-stock"  # Your Terraform Cloud org
-
+    organization = "cka-stock"
     workspaces {
-      name = "cka-stock"         # Your workspace name in Terraform Cloud
+      name = "cka-stock"
     }
   }
 
-
-
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
 }
 
+# Configures the Google provider with your project credentials.
 provider "google" {
   credentials = var.GOOGLE_CREDENTIALS
   project     = var.project_id
@@ -20,4 +33,27 @@ provider "google" {
   zone        = var.zone
 }
 
+# Fetches a client token for authentication.
+data "google_client_config" "default" {}
 
+# Fetches the GKE cluster details after it is created.
+data "google_container_cluster" "primary" {
+  name     = google_container_cluster.primary.name
+  location = var.region
+}
+
+# Configures the Kubernetes provider to connect to the GKE cluster.
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+# Configures the Helm provider to use the Kubernetes provider's configuration.
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
